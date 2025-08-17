2ff975114c02f6860adcc714203e1b8049a555cd
TommyNguyen <timsnguyen@hotmail.com>
2025-08-16 21:27:16 -0700

    add validateUser function

---
+++ Utils/validateUser.js
@@ -0,0 +1,36 @@
+const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+
+
+const validateUser = (inputData) => {
+  const errors = {};
+  if (
+    !inputData.name ||
+    typeof inputData.name !== "string" ||
+    inputData.name.trim().length === 0
+  ) {
+    errors.name = "Name is required and cannot be empty";
+  }
+
+  if (
+    !inputData.email ||
+    typeof inputData.email !== "string" ||
+    !emailRegex.test(inputData.email) ||
+    inputData.email.trim().length === 0
+  ) {
+    errors.email = "A valid email is required";
+  }
+
+  if (
+    !inputData.password ||
+    typeof inputData.password !== "string" ||
+    inputData.password.trim().length === 0
+  ) {
+    errors.password = "Password is required and cannot be empty";
+  }
+
+  return Object.keys(errors).length > 0 ? errors : null
+
+};
+
+
+export default validateUser;
