03a363113383f66bebccc59760ae636d58f64228
TommyNguyen <timsnguyen@hotmail.com>
2025-08-19 21:17:56 -0700

    add middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,20 @@
+const jwt = required("jsonwebtoken")
+
+const authMiddleware = (req, res, next) => {
+    const authHeader= req.headers("authorization")
+    const token = authHeader && authHeader.split(" ")[1]
+
+    if (!token) {
+        return res.status(403).json({ message: "Access denied, no tokens provided"})
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SCRET);
+        req.user = decoded
+        next();
+    } catch (err) {
+        return res.status(401).json({ message: "Invalid or expired token"})
+    }
+}
+
+module.exports = authMiddleware
